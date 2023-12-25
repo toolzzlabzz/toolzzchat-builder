389d709f3bf612457481b98dca3baf2fe4670423
lucasbuges <123869629+lucasbuges@users.noreply.github.com>
2023-12-25 09:13:36 -0300

    template tech update

--- apps/builder/src/features/theme/components/DefaultAvatar.tsx
+++ apps/builder/src/features/theme/components/DefaultAvatar.tsx
@@ -1,5 +1,5 @@
-import { Icon, IconProps } from '@chakra-ui/react'
-import React from 'react'
+import { Icon, IconProps } from "@chakra-ui/react";
+import React from "react";
 
 export const DefaultAvatar = (props: IconProps) => {
   return (
@@ -11,28 +11,11 @@ export const DefaultAvatar = (props: IconProps) => {
       data-testid="default-avatar"
       {...props}
     >
-      <mask id="mask0" x="0" y="0" mask-type="alpha">
-        <circle cx="37.5" cy="37.5" r="37.5" fill="#0042DA" />
-      </mask>
-      <g mask="url(#mask0)">
-        <rect x="-30" y="-43" width="131" height="154" fill="#0042DA" />
-        <rect
-          x="2.50413"
-          y="120.333"
-          width="81.5597"
-          height="86.4577"
-          rx="2.5"
-          transform="rotate(-52.6423 2.50413 120.333)"
-          stroke="#FED23D"
-          strokeWidth="5"
-        />
-        <circle cx="76.5" cy="-1.5" r="29" stroke="#FF8E20" strokeWidth="5" />
-        <path
-          d="M-49.8224 22L-15.5 -40.7879L18.8224 22H-49.8224Z"
-          stroke="#F7F8FF"
-          strokeWidth="5"
-        />
-      </g>
+      <image
+        href="https://media.discordapp.net/attachments/915691669234548746/1188803412045856838/robot.png"
+        width="100%"
+        height="100%"
+      />
     </Icon>
-  )
-}
+  );
+};
